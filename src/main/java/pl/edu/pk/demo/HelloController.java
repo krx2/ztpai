ea18904369ebda975636f0b4e1174f4b4c33c11b
Konrad Zebro <konrad.zebro@gmail.com>
2026-03-07 15:57:37 +0100

    initial commit

---
+++ src/main/java/pl/edu/pk/demo/HelloController.java
@@ -0,0 +1,12 @@
+package pl.edu.pk.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Witaj w Spring Boot!";
+    }
+}
